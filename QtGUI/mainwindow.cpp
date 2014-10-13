f92fa015b378faae4ef229271832268637483c09
yishikawa2 <yuki.ishikawa0313@gmail.com>
2014-10-13 11:43:47 -0700

    Create mainwindow.cpp

---
+++ QtGUI/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_authentication_clicked()
+{
+    SecDialog secdialog;
+    secdialog.setModal(true);
+    secdialog.exec();
+}
