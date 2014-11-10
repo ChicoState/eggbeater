9a3672900e7f265db8dc5df94e6713a1507dc29e
sadamsabc <sadamsabc@gmail.com>
2014-11-09 23:10:32 -0800

    Ability to read file and update progressbar
    ToDo:
    - figure how to access the system.io.getTempPath()
    - move components into invokeCLI

--- QtGUI/secdialog.cpp
+++ QtGUI/secdialog.cpp
@@ -1,5 +1,7 @@
 #include "secdialog.h"
 #include "ui_secdialog.h"
+#include <QTextStream>
+#include <iterator>
 
 SecDialog::SecDialog(QWidget *parent) :
     QDialog(parent),
@@ -152,35 +154,116 @@ void SecDialog::on_encrypt_clicked()
                                      .arg(file.errorString())
                                      );
             }
-
+            
             else
             {
-                bool discover = invoke->discoverDevice();
-                bool checkSession = invoke->sessionIsOpen();
-                if(discover==false && checkSession==false)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Session is not opened\nand\nDevice is not connected!"));
-                    return;
-                }
-                else if(discover==true && checkSession==false)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Session is not opened!"));
-                    return;
-                }
-                else if(discover==false && checkSession==true)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Device is not connected!"));
-                    return;
-                }
-                else
-                {
-                    invoke->startSession();
-                    invoke->encryptFiles(fileNames, "CFB");
-                    invoke->closeSession();
-                }
+
             }
         }
     }
+
+
+/****Start of progress bar update*******/
+    //testing code for updating the progress bar
+    //need to break into functions and integrate into "InvokeCLI"
+
+        QString temp = "TempComm.cpp"; //"C:\Users\sam\AppData\Local\TempComm.cpp"; //System::GetTempPath();
+        qint64 min=0;
+        qint64 max=100;
+        qint64 i=0;
+        qint64 progress=0;
+        qint64 curBlock=0;
+        qint64 positionTrack=0;
+        QString curFileCount=0;
+        QString maxCount=0;
+        ui->progressBar->setMaximum(max);
+        ui->progressBar->setMinimum(min);
+        QFile file(temp);
+        if(!file.open(QFile::ReadOnly))
+        {
+            QMessageBox::warning(this, tr("Application"), QDir::currentPath()+file.fileName(), tr("ok")); //tr("Cant find temp file \n")
+            return;
+        }
+        QTextStream in(&file);
+        while(!in.atEnd())
+        {
+            QString line=in.readLine();
+            QString::iterator it= line.begin();
+            QVector<char> curFileName;
+            if(it->isDigit()==true)
+            {
+                int linePos=0;
+                for(it=line.begin(); linePos<line.length(); linePos++)
+                {
+                    if(*it== "\"")
+                    {
+                        i=0;
+                        while(*it+1!="\"")
+                        {
+                            QByteArray temparray = (*it+1).toUtf8();
+                            curFileName[i] = temparray.at(i);
+                            i++;
+                        }
+                    }
+
+
+                    switch(positionTrack)
+                    {
+                        case 0:
+                            i=0;
+                            while(it->isDigit()==true)
+                            {
+                                if(i=0)
+                                    curFileCount=*it;
+                                else
+                                    curFileCount=curFileCount.append(*it);
+                                i++;
+                                it++;
+                            }
+                            positionTrack++;
+                        case 1:
+                            i=0;
+                            if(it->isDigit()==false)
+                                it++;
+                            while(it->isDigit()==true)
+                            {
+                                if(i=0)
+                                {
+                                    maxCount=*it;
+                                }
+                                else
+                                    maxCount=maxCount.append(*it);
+                                i++;
+                                it++;
+                                positionTrack++;
+                            }
+                        case 2:
+                            while(it->isDigit()==true)
+                            {
+                                it++;
+                                positionTrack++;
+                            }
+                        case 3:
+                            while(it->isDigit()==true)
+                            {
+                                it++;
+                                positionTrack++;
+                            }
+                    }
+                }
+            }
+        }
+        file.close();
+        qreal curC=curFileCount.toInt();
+        qreal maxC=maxCount.toInt();
+        progress=(curC/maxC)*100;
+
+        //use message box to test values
+        //QMessageBox::warning(this, sprogress, curFileCount, maxCount); //tr("Cant find temp file \n")
+        QString sprogress=QString::number(progress);
+        ui->progressBar->setValue(progress);
+
+/****End of progressbar update --SamAdams*******/
 
 /*
     QProgressDialog *dlg = new QProgressDialog(this);
@@ -200,10 +283,9 @@ void SecDialog::on_encrypt_clicked()
 
 void SecDialog::on_decrypt_clicked()
 {
-    if(fileNames.isEmpty() && folderName.isEmpty())
+    if(!fileNames.isEmpty() && !folderName.isEmpty())
     {
-        QMessageBox::warning(this, tr("The title"), tr("Slect File(s) or Folder(s)\nand Destination Folder."));
-        return;
+
     }
 
     else if(!fileNames.isEmpty() && folderName.isEmpty())
@@ -220,44 +302,7 @@ void SecDialog::on_decrypt_clicked()
 
     else
     {
-        for(int i=0; i<fileNames.size(); i++)
-        {
-            QFile file(fileNames.at(i));
-            if(!file.open(QFile::ReadOnly|QFile::Text))
-            {
-                QMessageBox::warning(this, tr("Application"),
-                                     tr("Cannot read file %1:\n%2")
-                                     .arg(fileNames.at(i))
-                                     .arg(file.errorString())
-                                     );
-            }
-
-            else
-            {
-                bool discover = invoke->discoverDevice();
-                bool checkSession = invoke->sessionIsOpen();
-                if(discover==false && checkSession==false)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Session is not opened\nand\nDevice is not connected!"));
-                    return;
-                }
-                else if(discover==true && checkSession==false)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Session is not opened!"));
-                    return;
-                }
-                else if(discover==false && checkSession==true)
-                {
-                    QMessageBox::warning(this, tr("The title"), tr("Device is not connected!"));
-                    return;
-                }
-                else
-                {
-                    invoke->startSession();
-                    invoke->decryptFiles(fileNames, "CFB");
-                    invoke->closeSession();
-                }
-            }
-        }
+        QMessageBox::warning(this, tr("The title"), tr("Slect File(s) or Folder(s)\nand Destination Folder."));
+        return;
     }
 }
