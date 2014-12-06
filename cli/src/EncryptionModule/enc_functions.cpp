926eb3ba7d474bae28c494e0eca824763edf9c74
swhitsett <kongzero32@gmail.com>
2014-12-06 01:35:21 -0800

    fixed buffer OverF + comments

--- cli/src/EncryptionModule/enc_functions.cpp
+++ cli/src/EncryptionModule/enc_functions.cpp
@@ -3,24 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
- 
+#include <stdio.h>
+#include "enc_functions.h"
+
+// crypto libaries found below
+// the "using" includes allows the code
+// to look a little readable.
 #include <cryptopp/cryptlib.h>
 using CryptoPP::Exception;
-using CryptoPP::PK_Encryptor;
-using CryptoPP::PK_Decryptor;
- 
-#include <cryptopp/sha.h>
-using CryptoPP::SHA256;
- 
-#include <cryptopp/secblock.h>
-using CryptoPP::SecByteBlock;
  
 #include <cryptopp/files.h>
 using CryptoPP::FileSink;
 using CryptoPP::FileSource;
- 
-#include <cryptopp/queue.h>
-using CryptoPP::ByteQueue;
  
 #include <cryptopp/hex.h>
 using CryptoPP::HexEncoder;
@@ -31,17 +25,9 @@ using CryptoPP::StringSink;
 using CryptoPP::StringSource;
 using CryptoPP::FileSink;
 using CryptoPP::FileSource;
-using CryptoPP::PK_EncryptorFilter;
-using CryptoPP::PK_DecryptorFilter;
 using CryptoPP::StreamTransformationFilter;
 using CryptoPP::AuthenticatedEncryptionFilter;
 using CryptoPP::AuthenticatedDecryptionFilter;
- 
-#include <cryptopp/osrng.h>
-using CryptoPP::AutoSeededRandomPool;
- 
-#include <cryptopp/dh.h>
-using CryptoPP::DH;
  
 #include <cryptopp/modes.h>
 using CryptoPP::CBC_Mode;
@@ -49,129 +35,138 @@ using CryptoPP::CBC_Mode;
 #include <cryptopp/gcm.h>
 using CryptoPP::GCM;
  
-#include <cryptopp/tea.h>
-using CryptoPP::TEA;
- 
 #include <cryptopp/aes.h>
 using CryptoPP::AES;
- 
-#include <cryptopp/pssr.h>
-using CryptoPP::PSS;
- 
-#include <cryptopp/rsa.h>
-using CryptoPP::RSA;
-using CryptoPP::RSASS;
- 
-#include <cryptopp/nbtheory.h>
-using CryptoPP::ModularExponentiation;
- 
-#include <cryptopp/eccrypto.h>
-using CryptoPP::ECDSA;
-using CryptoPP::ECP;
- 
-#include <cryptopp/oids.h>
-using CryptoPP::ASN1::secp521r1;
  
 #include <cryptopp/modes.h>
 using CryptoPP::OFB_Mode;
 using CryptoPP::CFB_Mode;
 using CryptoPP::Exception;
 
-#include "enc_functions.h"
-
 using namespace std;
 
 void enc_functions::encrypt_file(string encMode, string oFile, vector<uint8_t> pwordKey, vector<uint8_t> ivec)
 {
-
-   copy(pwordKey.begin(), pwordKey.end()-1, key);
-   copy(ivec.begin(), ivec.end()-1, iv);
-
-    string ofilename = oFile;
-    string outFile = oFile + ".egg";
-    string efilename = outFile;   
-    
-    ifstream ifile(oFile.c_str(), ios::binary);
-    ifstream::pos_type size = ifile.seekg(0, ios_base::end).tellg();
-    ifile.seekg(0, ios_base::beg);
-
-    string temp;
-    temp.resize(size);
-    ifile.read((char*)temp.data(), temp.size());
-   
-    if(encMode == "OFB")
+    //Allows encryption if input Vectors are of the required size
+    if( (pwordKey.size() == CryptoPP::AES::MAX_KEYLENGTH) && (ivec.size() == CryptoPP::AES::BLOCKSIZE) )
     {
-        OFB_Mode< AES >::Encryption e1;
-        e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
-       
-        StringSource( temp, true,
-                        new StreamTransformationFilter( e1,
-                        new FileSink( efilename.c_str() ))); 
-    }
-    else if(encMode == "CFB")
-    {
-        CFB_Mode< AES >::Encryption e1;
-        e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+        //pulls vectors into byte and initilization vectors
+        copy(pwordKey.begin(), pwordKey.end(), key);
+        copy(ivec.begin(), ivec.end(), iv);
 
-        StringSource( temp, true,
-                        new StreamTransformationFilter( e1,
-                        new FileSink( efilename.c_str() ))); 
-    }
-    else if(encMode == "GCM")
-    {
-        GCM< AES >::Encryption e1;
-        e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
-       
-        StringSource ss1( temp, true,
-                        new AuthenticatedEncryptionFilter( e1,
-                        new FileSink( efilename.c_str() )));
+        //Adds .egg to siginifiy that the file is encrypted by eggbeater
+        string ofilename = oFile;
+        string outFile = oFile + ".egg";
+        string efilename = outFile;   
+
+        // creates a copy of the input file
+        // and converts it to binary for encryption.
+        // Helps avoid the creation of corrupted files
+        // durring decryption process
+        ifstream ifile(oFile.c_str(), ios::binary);
+        ifstream::pos_type size = ifile.seekg(0, ios_base::end).tellg();
+        ifile.seekg(0, ios_base::beg);
+
+        string temp;
+        temp.resize(size);
+        ifile.read((char*)temp.data(), temp.size());
+
+        if(encMode == "OFB")
+        {
+            OFB_Mode< AES >::Encryption e1;
+            e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+           
+            StringSource( temp, true,
+                            new StreamTransformationFilter( e1,
+                            new FileSink( efilename.c_str() ))); 
+        }
+        else if(encMode == "CFB")
+        {
+            CFB_Mode< AES >::Encryption e1;
+            e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+
+            StringSource( temp, true,
+                            new StreamTransformationFilter( e1,
+                            new FileSink( efilename.c_str() ))); 
+        }
+        else if(encMode == "GCM")
+        {
+            GCM< AES >::Encryption e1;
+            e1.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+           
+            StringSource ss1( temp, true,
+                            new AuthenticatedEncryptionFilter( e1,
+                            new FileSink( efilename.c_str() )));
+        }
+        else
+        {
+            cerr << "[ERROR] Invalid Mode" <<endl;
+        }
     }
     else
     {
-        cerr << "Invalid Mode" <<endl;
+        cerr<<"[ERROR] Key size is insufficient"<<endl;
     }
 }
 
 void enc_functions::decrypt_file(string decMode, string efile, vector<uint8_t> pwordKey, vector<uint8_t> ivec)  //keep hash
 {   
-
-    string efilename = efile;
-    efile.erase(efile.end()-4, efile.end());
-    string rfilename = efile;
-
-    copy(pwordKey.begin(), pwordKey.end()-1, key);
-    copy(ivec.begin(), ivec.end()-1, iv);
-
-    if(decMode == "OFB")
+    //Allows encryption if input Vectors are of the required size
+    if( (pwordKey.size() == CryptoPP::AES::MAX_KEYLENGTH) && (ivec.size() == CryptoPP::AES::BLOCKSIZE) )
     {
-        OFB_Mode< AES >::Decryption d2;
-        d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
-       
-        FileSource( efilename.c_str(), true,
-                        new StreamTransformationFilter( d2,
-                        new FileSink( rfilename.c_str() ))); 
-    }
-    else if(decMode == "CFB")
-    {
-        CFB_Mode< AES >::Decryption d2;
-        d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
-       
-        FileSource( efilename.c_str(), true,
-                        new StreamTransformationFilter( d2,
-                        new FileSink( rfilename.c_str() )));
-    }
-    else if(decMode == "GCM")
-    {
-        GCM< AES >::Decryption d2;
-        d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
-       
-        FileSource fs2( efilename.c_str(), true,
-                        new AuthenticatedDecryptionFilter( d2,
-                        new FileSink( rfilename.c_str() ),
-                        AuthenticatedDecryptionFilter::THROW_EXCEPTION));
+        //Checks if the ending of the provided filename
+        //is an eggbeater encrypted file, otherwise decryption
+        //will halt.
+        if(!efile.compare(efile.size() - 4, 4, ".egg") == 0)
+        {
+           cerr<<" [ERROR] The File provided is not an Eggbeater encrypted file \n";
+           cerr<<"         Please choose a .egg file to decrypt \n";
+           return;
+        }
+
+        //Removes the .egg from the file type provided
+        string efilename = efile;
+        efile.erase(efile.end()-4, efile.end());
+        string rfilename = efile;
+
+        copy(pwordKey.begin(), pwordKey.end()-1, key);
+        copy(ivec.begin(), ivec.end()-1, iv);
+
+        if(decMode == "OFB")
+        {
+            OFB_Mode< AES >::Decryption d2;
+            d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+           
+            FileSource( efilename.c_str(), true,
+                            new StreamTransformationFilter( d2,
+                            new FileSink( rfilename.c_str() ))); 
+        }
+        else if(decMode == "CFB")
+        {
+            CFB_Mode< AES >::Decryption d2;
+            d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+           
+            FileSource( efilename.c_str(), true,
+                            new StreamTransformationFilter( d2,
+                            new FileSink( rfilename.c_str() )));
+        }
+        else if(decMode == "GCM")
+        {
+            GCM< AES >::Decryption d2;
+            d2.SetKeyWithIV( key, sizeof(key), iv, sizeof(iv) );
+           
+            FileSource fs2( efilename.c_str(), true,
+                            new AuthenticatedDecryptionFilter( d2,
+                            new FileSink( rfilename.c_str() ),
+                            AuthenticatedDecryptionFilter::THROW_EXCEPTION));
+        }
+        else
+        {
+            cerr << " [ERROR] Decryption Error" <<endl;
+        }
     }
     else
     {
-        cerr << "Decryption Error" <<endl;
+        cerr<<" [ERROR] Key size is insufficient"<<endl;
     }
 }
--- cli/src/EncryptionModule/main.cpp
+++ cli/src/EncryptionModule/main.cpp
@@ -13,15 +13,15 @@ int main(int argc, char* argv[])
     vector<uint8_t> k;
     vector<uint8_t> v;
 
-    for (int i=0; i<64; i++)
-    	k.push_back(0);
     for (int i=0; i<32; i++)
+    	k.push_back(0);
+    for (int i=0; i<16; i++)
     	v.push_back(0);
 
-    if(argc == 6 && action == "-enc")
+    if(argc == 4 && action == "-enc")
         e.encrypt_file(argv[2], argv[3], k, v); //argv3 is the provided key
 
-    else if(argc == 6 && action == "-dec")
+    else if(argc == 4 && action == "-dec")
         e.decrypt_file(argv[2], argv[3], k, v);
 
     else
