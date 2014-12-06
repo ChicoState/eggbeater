07a6758c14a3d415e4d9fbc6c3a440915613bdee
yishikawa2 <yuki.ishikawa0313@gmail.com>
2014-12-06 14:52:16 -0800

    fixed bug

--- QtGUI/selectcipher.cpp
+++ QtGUI/selectcipher.cpp
@@ -38,16 +38,28 @@ int selectCipher::getI()
 void selectCipher::on_CFB_Button_clicked()
 {
     cipherMode = "cfb";
+    ui->OFB_Button->setAutoExclusive(false);
+    ui->OFB_Button->setChecked(false);
+    ui->GCM_Button->setAutoExclusive(false);
+    ui->GCM_Button->setChecked(false);
 }
 
 void selectCipher::on_OFB_Button_clicked()
 {
     cipherMode = "ofb";
+    ui->CFB_Button->setAutoExclusive(false);
+    ui->CFB_Button->setChecked(false);
+    ui->GCM_Button->setAutoExclusive(false);
+    ui->GCM_Button->setChecked(false);
 }
 
 void selectCipher::on_GCM_Button_clicked()
 {
     cipherMode = "gcm";
+    ui->CFB_Button->setAutoExclusive(false);
+    ui->CFB_Button->setChecked(false);
+    ui->OFB_Button->setAutoExclusive(false);
+    ui->OFB_Button->setChecked(false);
 }
 
 void selectCipher::on_OK_Button_clicked()
