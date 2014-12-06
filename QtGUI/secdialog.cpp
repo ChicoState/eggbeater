2365b322482cc44c50eb70cfdd258138c5def01d
yishikawa2 <yuki.ishikawa0313@gmail.com>
2014-12-06 02:37:24 -0800

    Update secdialog.cpp

--- QtGUI/secdialog.cpp
+++ QtGUI/secdialog.cpp
@@ -186,7 +186,7 @@ void SecDialog::on_encrypt_clicked()
             QProcess* proc = new QProcess(this);
             invoke->encryptFiles(fileNames, folderName, "cfb", proc);
             //invoke->closeSession();
-            t->start(); //crushed here
+            t->start(); //crashed here
         }
     }
     //pd->showNormal();
@@ -243,7 +243,7 @@ void SecDialog::on_decrypt_clicked()
             QProcess* proc = new QProcess(this);
             invoke->decryptFiles(fileNames, folderName, "cfb", proc);
             //invoke->closeSession();
-            t->start(); //crushed here
+            t->start(); //crashed here
         }
     }
 }
