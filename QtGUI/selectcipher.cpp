e2e618e18df457c4be833d346ee6e19e9d85a7f4
yishikawa2 <yuki.ishikawa0313@gmail.com>
2014-12-06 05:22:16 -0800

    Update selectcipher.cpp

--- QtGUI/selectcipher.cpp
+++ QtGUI/selectcipher.cpp
@@ -16,6 +16,15 @@ QString selectCipher::getCipherMode()
 {
     return cipherMode;
 }
+void selectCipher::resetRadioButtons()
+{
+    ui->CFB_Button->setAutoExclusive(false);
+    ui->CFB_Button->setChecked(false);
+    ui->OFB_Button->setAutoExclusive(false);
+    ui->OFB_Button->setChecked(false);
+    ui->GCM_Button->setAutoExclusive(false);
+    ui->GCM_Button->setChecked(false);
+}
 void selectCipher::on_CFB_Button_clicked()
 {
     cipherMode = "cfb";
